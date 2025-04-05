8768b7d8ad6112d4e83d074af8db4f1ac831d487
Caio Henrique <caiohenriquepassos090@gmail.com>
2025-04-04 23:59:27 -0300

    integração a API

--- src/Pages/LivrosDoados/LivrosDoados.jsx
+++ src/Pages/LivrosDoados/LivrosDoados.jsx
@@ -1,19 +1,31 @@
 import livro from "../../assets/livrored.png"
 import S from './livrosdoados.module.scss'
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 export default function LivrosDoados() {
+
+    const [livros, setLivros] = useState([])
+
+    const getLivros = async () => {
+        const resposta = await axios.get("https://api-t1-e296.onrender.com/livros")
+        setLivros(resposta.data)
+    }
+    
+    useEffect(() => {
+        getLivros()
+    }, [])
+
     return(
         <section className={S.livrosdoados}>
             <h2 className={S.titulo2}>Livros Doados</h2>
             <section className={S.livros}>
-                <section className={S.container}>
-                    <img src={livro} alt="" />
-                    <section className={S.textos}>
-                        <p>O protagonista</p>
-                        <p>Susanne Andrade</p>
-                        <p>Ficção</p>
+                {livros.map((item) => (
+                    <section className={S.container}>
+                        <h3>{item.titulo}</h3>
+                        <img scr= {item.image_url} alt={item.titulo}></img>
                     </section>
-                </section>
+                ))}
             </section>
         </section>
 
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,8 +1,43 @@
 import livro_logo from "../../assets/Vector.png"
-
 import S from './querodoar.module.scss'
+import { useState } from "react"
+import axios from "axios"
 
 export default function QueroDoar() {
+
+    const [titulo,setTitulo] = useState("")
+    const [categoria,setCategoria] = useState("")
+    const [autor,setAutor] = useState("")
+    const [image_url,setImage_url] = useState("")
+
+    const capturaTitulo = (e) =>{
+        setTitulo(e.target.value)
+    }
+
+    const capturaCategoria = (e) =>{
+        setCategoria(e.target.value)
+    }
+
+    const capturaAutor = (e) =>{
+        setAutor(e.target.value)
+    }
+
+    const capturaImg = (e) =>{
+        setImage_url(e.target.value)
+    }
+
+    const envioDados = async() =>{
+
+        const dadosPEnviar = {
+            titulo,
+            categoria,
+            autor,
+            image_url
+        }
+        await axios.post("https://api-t1-e296.onrender.com/doar", dadosPEnviar)
+
+    }
+
     return(
         <section className={S.querodoar}>
             <p className={S.Texte}>Por favor, preencha o formulário com suas informações e as informações do Livro.</p>
@@ -12,13 +47,13 @@ export default function QueroDoar() {
                     <h2>Informações do Livro</h2>
                 </div>
                 <section className={S.inputss}>
-                    <input className={S.barra} type="text" placeholder="Título" />
-                    <input className={S.barra} type="text" placeholder="Categoria" />
-                    <input className={S.barra} type="text" placeholder="Autor" />
-                    <input className={S.barra} type="text" placeholder="Link da Imagem" />
+                    <input className={S.barra} type="text" placeholder="Título" onChange={capturaTitulo} />
+                    <input className={S.barra} type="text" placeholder="Categoria" onChange={capturaCategoria}/>
+                    <input className={S.barra} type="text" placeholder="Autor" onChange={capturaAutor}/>
+                    <input className={S.barra} type="text" placeholder="Link da Imagem" onChange={capturaImg}/>
                 </section>
                 <section className={S.butao}>
-                    <button className={S.butal}>
+                    <button className={S.butal} type = "button" onClick={envioDados} >
                         Doar
                     </button>
                 </section>
